1b7dec4af91a3d2a1ae9b708221a97763803bab2
杨阳 <you@example.com>
2018-06-26 10:47:30 +0800

    1

--- src/assets/js/jwt.share.js
+++ src/assets/js/jwt.share.js
@@ -1,4 +1,4 @@
-import wx from 'weixin-js-sdk'
+var wx = require('weixin-js-sdk')
 import * as apis from './jwt.apis'
 
 
@@ -6,43 +6,61 @@ import * as apis from './jwt.apis'
  * 初始化微信SDK
  * @type {string[]}
  */
-export const jsApiList = ['onMenuShareAppMessage'];    //js接口
-export const curUrl = 'http://139.129.12.93:3102/wx';    //要传的链接
-export const isDebug = false; //开启调试模式
-
-
-//为了安全起见，微信的appId等配置信息通过后台获取
+export function initWxJsAPI(){
+  //const jsApiList = ['onMenuShareAppMessage'];  //js接口
+  const curUrl = location.href.split('#')[0];    //要传的链接
+  const isDebug = false; //开启调试模式
+  let deferred = $.Deferred();
+  let promise = deferred.promise();
+  //为了安全起见，微信的appId等配置信息通过后台获取
 //所以需要先发异步请求
-apis.tSignature(curUrl).then(function(res){
-  if(res.message == 'ok'){
-    let resData  = res.result.main_data.data[0];
-    console.log(resData)
-    wx.config({
-      debug: isDebug, //开启调试模式
-      appId: resData.appid, // 必填，公众号的唯一标识
-      timestamp: resData.timestamp, // 必填，生成签名的时间戳
-      nonceStr: resData.nonceStr, // 必填，生成签名的随机串
-      signature: resData.signature,// 必填，签名
-      jsApiList: jsApiList //必填，需要使用的JS接口列表
-    });
-  }
+  setTimeout(function () {
+    apis.tSignature(curUrl).then(function(res){
+      if(res.message == 'ok'){
+        let resData  = res.result.main_data.data[0];
+        console.log(resData)
+        wx.config({
+          debug: isDebug, //开启调试模式
+          appId: resData.appid, // 必填，公众号的唯一标识
+          timestamp: resData.timestamp, // 必填，生成签名的时间戳
+          nonceStr: resData.nonceStr, // 必填，生成签名的随机串
+          signature: resData.signature,// 必填，签名
+          jsApiList: ['onMenuShareAppMessage'] //必填，需要使用的JS接口列表
+        });
+        wx.ready(function () {
+          deferred.resolve(wx);
+        })
 
-
-});
-
-
-export  function onMenuShareAppMessage () {
-    wx.onMenuShareAppMessage({
-      title: '普惠通数据信息技术有限公司', // 分享标题
-      desc: '这是分享的描述', // 分享描述
-      link: 'http://139.129.12.93:3102/wx', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-      imgUrl: 'https://www.phtfdata.com/web6/images/logo-logo.png', // 分享图标
-      type: 'link', // 分享类型,music、video或link，不填默认为link
-      dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-      success: function () {
-// 用户点击了分享后执行的回调函数
-
+      }else {
+        $.showphtModal({
+          withOneButton:0,
+          errorMsg:"微信初始化失败!",
+          type: 1
+        })
       }
-    });
+    },60);
+  })
+  return promise;
 
 }
+
+//分享给朋友
+export function onMenuShareAppMessage(){
+  console.log(1111)
+  wx.onMenuShareAppMessage({
+    title: '普惠通数据信息技术有限公司', // 分享标题
+    desc: '这是分享的描述', // 分享描述
+    link: 'http://192.168.1.108:8080/wx', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+    imgUrl: 'https://www.phtfdata.com/web6/images/logo-logo.png', // 分享图标
+    type: 'link', // 分享类型,music、video或link，不填默认为link
+    dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+    success: function () {
+// 用户点击了分享后执行的回调函数
+    }
+  });
+}
+
+
+
+
+
--- src/pages/share/share.vue
+++ src/pages/share/share.vue
@@ -1,22 +1,24 @@
 <template>
   <div>
-   <button @click="shareF()">分享给朋友</button>
+   <button @click="share()">分享给朋友</button>
   </div>
 </template>
 
 <script>
-  import '../../assets/js/jwt.share';
-  import {onMenuShareAppMessage} from '../../assets/js/jwt.share'
+ import * as wxShare from '../../assets/js/jwt.share'
 export default {
    data () {
     return {
 
     }
 },
+  mounted(){
+    wxShare.initWxJsAPI().then(function () {
+      wxShare.onMenuShareAppMessage()
+    })
+  },
   methods:{
-    shareF(){
-      onMenuShareAppMessage()
-    },
+
 
   }
 
